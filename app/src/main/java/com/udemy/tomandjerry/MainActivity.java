45486f0ec786b3f05486487fdd81c59f2c4dcf4a
Gregorii work <tijester@ukr.net>
2020-05-27 14:53:01 +0300

    Initial commit

---
+++ app/src/main/java/com/udemy/tomandjerry/MainActivity.java
@@ -0,0 +1,38 @@
+package com.udemy.tomandjerry;
+
+        import androidx.appcompat.app.AppCompatActivity;
+
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    boolean isImageVisible = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void eraseTaJ(View view) {
+
+        if (isImageVisible){
+            ImageView taj1ImageView = findViewById(R.id.taj1ImageView);
+            taj1ImageView.animate().alpha(0).setDuration(3000);
+
+            ImageView taj2ImageView = findViewById(R.id.taj2ImageView);
+            taj2ImageView.animate().alpha(1).setDuration(3000);
+            isImageVisible = false;
+        } else {
+            ImageView taj1ImageView = findViewById(R.id.taj1ImageView);
+            taj1ImageView.animate().alpha(1).setDuration(3000);
+
+            ImageView taj2ImageView = findViewById(R.id.taj2ImageView);
+            taj2ImageView.animate().alpha(0).setDuration(3000);
+            isImageVisible = true;
+        }
+
+    }
+}
