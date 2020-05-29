24f034251d85c38e86fb927d3f1ae3bdbc2f4595
Gregorii work <tijester@ukr.net>
2020-05-29 14:24:03 +0300

    scale image

--- app/src/main/java/com/udemy/tomandjerry/MainActivity.java
+++ app/src/main/java/com/udemy/tomandjerry/MainActivity.java
@@ -14,23 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void eraseTaJ(View view) {
 
-        if (isImageVisible){
+      if (isImageVisible){
             ImageView taj1ImageView = findViewById(R.id.taj1ImageView);
-            taj1ImageView.animate().alpha(0).setDuration(3000);
+            taj1ImageView.animate().alpha(0).rotation(taj1ImageView.getRotation() + 3600).scaleY(0).scaleX(0).setDuration(3000);
 
             ImageView taj2ImageView = findViewById(R.id.taj2ImageView);
-            taj2ImageView.animate().alpha(1).setDuration(3000);
+            taj2ImageView.animate().alpha(1).rotation(taj2ImageView.getRotation() + 3600).scaleY(1).scaleX(1).setDuration(3000);
             isImageVisible = false;
         } else {
             ImageView taj1ImageView = findViewById(R.id.taj1ImageView);
-            taj1ImageView.animate().alpha(1).setDuration(3000);
+            taj1ImageView.animate().alpha(1).rotation(taj1ImageView.getRotation() + 3600).scaleY(1).scaleX(1).setDuration(3000);
 
             ImageView taj2ImageView = findViewById(R.id.taj2ImageView);
-            taj2ImageView.animate().alpha(0).setDuration(3000);
+            taj2ImageView.animate().alpha(0).rotation(taj2ImageView.getRotation() + 3600).scaleY(0).scaleX(0).setDuration(3000);
             isImageVisible = true;
         }
     }
